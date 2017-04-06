047ee1b6a939e013dd9670fa3f8ea9d0ff7a133a
MStenkaer <mstenk16@student.aau.dk>
2017-04-06 11:31:45 +0200

    Opstart af algoritme

---
+++ P2 Projekt/Algoritma/Stoppested.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma
+{
+    struct coordinate
+    {
+        int xCoordinate;
+        int yCoordinate;
+    }
+
+    struct passagerAfPå
+    {
+        DateTime tid;
+        int afstigninger;
+        int påstigninger;
+    }
+
+    class Stoppested
+    {
+        List<passagerAfPå> AfPåstigningerList = new List<passagerAfPå>();
+        public Stoppested (params DateTime[] besøgstider)
+        {
+            foreach(DateTime tidspunkt in besøgstider)
+            {
+                
+            }
+        }
+
+        public int stoppestedID { get; set; }
+        public coordinate stoppestedLok { get; set; }
+
+    }
+}
