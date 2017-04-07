2dc7982e9487e971ab361b7eb66b09e73272fa59
MStenkaer <mstenk16@student.aau.dk>
2017-04-07 11:26:02 +0200

    Lavet nogle classes til forskellige objekter.

---
+++ P2 Projekt/Algoritma/Bus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritme
+{
+    class Bus
+    {
+        public int busID;
+        public GPS placering;
+        public int passengersTotal;
+        public int capacity;
+        public Rute rute;
+
+        public Bus (int busID, int capacity, Rute rute)
+        {
+            this.busID = busID;
+            this.capacity = capacity;
+            this.rute = rute;
+        }
+
+    }
+}
