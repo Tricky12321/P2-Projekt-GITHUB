3e7bca34e1d7af198d0938d60a5c77e53d220020
Anantonon <hezarkit@msn.com>
2017-04-07 10:30:16 +0200

    Lavede testknap

--- P2 Projekt/AndroidApp/MainActivity.cs
+++ P2 Projekt/AndroidApp/MainActivity.cs
@@ -1,10 +1,12 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.Widget;
 using Android.OS;
 
 namespace AndroidApp
 {
-    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "LasseogAnton", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -12,7 +14,15 @@ namespace AndroidApp
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            EditText FuckDigTekst = FindViewById<EditText>(Resource.Id.Fuckdigtekst);
+            Button KnapÆndrer = FindViewById<Button>(Resource.Id.KnapAendrer);
+            KnapÆndrer.Click += (object sender, EventArgs e) =>
+            {
+                FuckDigTekst.Text = "Elsker dig";
+            };
+
         }
     }
 }
--- P2 Projekt/AndroidApp/Resources/Resource.Designer.cs
+++ P2 Projekt/AndroidApp/Resources/Resource.Designer.cs
@@ -57,6 +57,25 @@ namespace AndroidApp
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Fuckdigtekst = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int KnapAendrer = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
