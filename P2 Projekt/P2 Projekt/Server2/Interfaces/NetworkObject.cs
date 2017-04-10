e535be0a2fd2dbf915af8ab8cb47c5b6c4943425
Thue Iversen <Thue-V-Iversen@hotmail.com>
2017-04-10 13:13:20 +0200

    123

---
+++ P2 Projekt/P2 Projekt/Server2/Interfaces/NetworkObject.cs
@@ -0,0 +1,16 @@
+﻿public interface NetworkObject
+{
+    void Start();
+
+    int GetID();
+
+    string GetTableName();
+
+    string[] GetCollumsDB();
+
+    string[] GetValues();
+
+    string GetIDCollumName();
+
+    void GetUpdate(TableDecode TableContent);
+}
