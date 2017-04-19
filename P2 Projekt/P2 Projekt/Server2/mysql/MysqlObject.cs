5cc4f5311641fee6bce4a214e49ab9c328ea9b43
Thue Iversen <Thue-V-Iversen@hotmail.com>
2017-04-19 14:16:23 +0200

    Lavede opdateringer til Mysql og gjort det muligt at uploade testobject til databasen
    Jeg har også opdateret logging systemet til at logge meget mere data

---
+++ P2 Projekt/P2 Projekt/Server2/mysql/MysqlObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public abstract class MysqlObject : NetworkObject
+{
+
+    public abstract void Start();
+
+    public string[] GetCollumsDB()
+    {
+        return MysqlControls.SelectOne(this.GetTableName()).RowData[0].Colums.ToArray();
+    }
+
+    public abstract string[] GetValues();
+
+    public abstract string[] GetValuesDB();
+
+    public abstract int GetID();
+
+    public abstract string GetIDCollumName();
+
+    public abstract string GetTableName();
+
+    public abstract void GetUpdate();
+
+    public abstract void Update(TableDecode TableContent);
+
+    public abstract TableDecode GetThisFromDB();
+
+    public abstract string WhereID();
+
+    public void UploadToDatabase()
+    {
+        
+        if (MysqlControls.IsIDInDatabase(this))
+        {
+            MysqlControls.UpdateWhere(GetTableName(), GetCollumsDB(), GetValues(), WhereID());
+        } else
+        {
+            string[] Colums = MysqlControls.GetColumsFromDBRaw(GetTableName());
+            string[] Values = GetValues();
+            MysqlControls.Insert(GetTableName(), Colums, GetValues());
+        }
+    }
+}
--- P2 Projekt/P2 Projekt/Server2/utilities/Log.cs
+++ P2 Projekt/P2 Projekt/Server2/utilities/Log.cs
@@ -7,11 +7,11 @@ public static class Log
 {
     public static void LogData(string funcstring, string description)
     {
-        MysqlControls.Insert("logging", new string[] { "server_os", "function", "description" }, new string[] { Utilities.GetOS(), funcstring, description });
+        MysqlControls.Insert("logging", new string[] { "server_os", "function", "description" }, new string[] { Utilities.GetOS(), funcstring, description }, true);
     }
 
     public static void LogData(string description)
     {
-        MysqlControls.Insert("logging", new string[] { "server_os", "function", "description" }, new string[] { Utilities.GetOS(), "NULL", description });
+        MysqlControls.Insert("logging", new string[] { "server_os", "function", "description" }, new string[] { Utilities.GetOS(), "NULL", description }, true);
     }
 }
