284102cfa51f1b0cf7eb1388b4be879f11d8ba2d
Matin Simonsen <Martin-ezzi@hotmail.com>
2017-04-24 11:09:26 +0200

    Lavet på objekter til database

--- P2 Projekt/P2 Projekt/Server2/Objecter/AfPåTidCombi.cs
+++ P2 Projekt/P2 Projekt/Server2/Objecter/AfPåTidCombi.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 public class AfPåTidCombi : MysqlObject
 {
+    public int ID;
     public Tidspunkt Tidspunkt;
     public int afstigninger;
     public int påstigninger;
@@ -13,5 +14,67 @@ public class AfPåTidCombi : MysqlObject
     public override string ToString()
     {
         return Tidspunkt.ToString();
+    }
+
+    public override void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int GetID()
+    {
+        return this.ID;
+    }
+
+    public override string GetIDCollumName()
+    {
+        return "ID";
+    }
+
+    public override string GetTableName()
+    {
+        return "AfpåTid";
+    }
+
+    public override void GetUpdate()
+    {
+        Update(MysqlControls.SelectAllWhere(GetTableName(), WhereID()));
+    }
+
+    public override void Update(TableDecode TableContent)
+    {
+        ID = Convert.ToInt32(TableContent.RowData[0].Values[0]);                            // INT 32 ID
+        afstigninger = Convert.ToInt32(TableContent.RowData[0].Values[1]);                  // INT 32
+        påstigninger = Convert.ToInt32(TableContent.RowData[0].Values[2]);
+        Tidspunkt = new Tidspunkt();
+        Tidspunkt.hour = Convert.ToInt32(TableContent.RowData[0].Values[3]);
+        Tidspunkt.minute = Convert.ToInt32(TableContent.RowData[0].Values[4]);
+    }
+
+    public override string[] GetValues()
+    {
+        List<string> Output = new List<string>();
+        Output.Add(ID.ToString());
+        Output.Add(afstigninger.ToString());
+        Output.Add(påstigninger.ToString());
+        Output.Add(Tidspunkt.hour.ToString());
+        Output.Add(Tidspunkt.minute.ToString());
+
+        return Output.ToArray();
+    }
+
+    public override string[] GetValuesDB()
+    {
+        return GetThisFromDB().RowData[0].Values.ToArray();
+    }
+
+    public override TableDecode GetThisFromDB()
+    {
+        return MysqlControls.SelectAllWhere(GetTableName(), WhereID());
+    }
+
+    public override string WhereID()
+    {
+        return $"`{GetIDCollumName()}`={GetID()}";
     }
 }
--- P2 Projekt/P2 Projekt/Server2/Objecter/GPS.cs
+++ P2 Projekt/P2 Projekt/Server2/Objecter/GPS.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class GPS : MysqlObject
+public class GPS
 {
     public double xCoordinate;
     public double yCoordinate;
