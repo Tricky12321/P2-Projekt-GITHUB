022d21217da2c943a314a16074c4e55da6d27d73
Anantonon <hezarkit@msn.com>
2017-05-08 13:09:19 +0200

    Fjernet ubrugelig knap, rengjort mainactivity-kode

--- P2 Projekt/AndroidApp/Activities (Sider)/MainActivity.cs
+++ P2 Projekt/AndroidApp/Activities (Sider)/MainActivity.cs
@@ -17,19 +17,12 @@ namespace AndroidApp
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            TextView FuckDigTekst = FindViewById<TextView>(Resource.Id.Fuckdigtekst);
-            Button KnapÆndrer = FindViewById<Button>(Resource.Id.KnapAendrer);
+            /* Gør knapper til henholdsvis resultat-aktiviteten og favorit-aktiviteten klar */
             Button resultButton = FindViewById<Button>(Resource.Id.ResultButton);
-            resultButton.Click += delegate { ShowResults(); };
+            resultButton.Click += delegate { ShowResultsActivity(); };
 
             Button FavoriteButton = FindViewById<Button>(Resource.Id.FavButton);
-            FavoriteButton.Click += delegate { ShowFavorites(); };
-            
-            KnapÆndrer.Click += (object sender, EventArgs e) =>
-            {
-                RealClient Test = new RealClient();
-                List<NetworkObject> ReturnObjs = Test.RequestAllWhere(ObjectTypes.BusStop, "None");
-            };
+            FavoriteButton.Click += delegate { ShowFavoritesActivity(); };
 
             /* Gør rejsetidspunkt-knapperne klar */
             ny_tid = FindViewById<TextView>(Resource.Id.nytid);
@@ -50,7 +43,6 @@ namespace AndroidApp
             CurrentTime = DateTime.Now;
             hours = CurrentTime.Hour;
             minutes = CurrentTime.Minute;
-
             UpdateDisplay(hours, minutes);
         }
 
@@ -68,49 +60,6 @@ namespace AndroidApp
         }
 
         string StoppestedInputString;
-        static string[] COUNTRIES = new string[] {
-  "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
-  "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
-  "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
-  "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-  "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-  "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
-  "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-  "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-  "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
-  "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
-  "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-  "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-  "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-  "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-  "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
-  "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar",
-  "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau",
-  "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-  "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-  "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-  "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-  "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
-  "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
-  "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-  "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-  "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-  "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-  "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-  "Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena",
-  "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-  "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal",
-  "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-  "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea",
-  "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-  "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
-  "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-  "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-  "Ukraine", "United Arab Emirates", "United Kingdom",
-  "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan",
-  "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
-  "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
-};
 
         /* Rejsetidspunkt-variable */
         TextView ny_tid;
@@ -125,7 +74,7 @@ namespace AndroidApp
             dialog.Show(FragmentManager, null);
         }
 
-        void ShowResults()
+        void ShowResultsActivity()
         {
             List<string> stopOgTid = new List<string>() { StoppestedInputString, hours.ToString(), minutes.ToString() };
             var intent = new Intent(this, typeof(BusResults));
@@ -133,7 +82,7 @@ namespace AndroidApp
             StartActivity(intent);
         }
 
-        void ShowFavorites()
+        void ShowFavoritesActivity()
         {
             var intent = new Intent(this, typeof(FavoriteBusses));
             StartActivity(intent);
@@ -143,7 +92,7 @@ namespace AndroidApp
         {
             UpdateDisplay(hourOfDay, minute);
         }
-
+        
         void UpdateDisplay(int selectedHours, int selectedMinutes)
         {
             if (selectedHours < 10 || selectedMinutes < 10)
